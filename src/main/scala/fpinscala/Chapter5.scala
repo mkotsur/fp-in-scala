3a421ab34a888872fc1545e2b5ba24c03ea8569a
Mike Kotsur <miccots@gmail.com>
2017-01-29 18:39:06 +0100

    Exercise 5.7 done

--- src/main/scala/fpinscala/Chapter5.scala
+++ src/main/scala/fpinscala/Chapter5.scala
@@ -1,14 +1,21 @@
 package fpinscala
 
+import fpinscala.Chapter5.Stream.cons
+
 import scala.annotation.tailrec
 
 object Chapter5 {
 
   sealed trait Stream[+A] {
-    def headOption: Option[A] = this match {
+
+    // This has been reimplemented using foldRight()
+    def headOption_ : Option[A] = this match {
       case Empty => None
       case Cons(h, t) => Some(h())
     }
+
+    // foldRight() - based implementation
+    def headOption: Option[A] = this.foldRight[Option[A]](None)((a, _) => Some(a))
 
     // Exercice 5.1
     def toList: List[A] = this match {
@@ -18,6 +25,7 @@ object Chapter5 {
 
     def take(n: Int): Stream[A] = this match {
       case _ if n == 0 => Empty
+      case Cons(h, _) if n == 1 => Cons(h, () => Empty)
       case Cons(h, t) => Cons(h, () => t().take(n - 1))
       case Empty => throw new IllegalStateException("Can't take elements from empty stream")
     }
@@ -29,12 +37,46 @@ object Chapter5 {
       case Cons(_, t) => t().drop(n - 1)
     }
 
-    def takeWhile(p: A => Boolean): Stream[A] = this match {
+    // This has been reimplemented using foldRight()
+    def takeWhile_(p: A => Boolean): Stream[A] = this match {
       case Empty => Empty
       case Cons(h, t) => {
         if (p(h())) Cons(h, () => t().takeWhile(p)) else Empty
       }
     }
+
+    // foldRight() - based implementation
+    def takeWhile(p: A => Boolean): Stream[A] = this.foldRight[Stream[A]](Empty)((a, b) => {
+      if (p(a)) Cons(() => a, () => b.takeWhile(p))
+      else b
+    })
+
+    def foldRight[B](z: => B)(f: (A, => B) => B): B = this match {
+      case Cons(h, t) => f(h(), t().foldRight(z)(f))
+      case _ => z
+    }
+
+    def forAll(p: A => Boolean): Boolean =
+      foldRight(true)((a, b) => p(a) && b)
+
+    def map[B](f: A => B): Stream[B] =
+      foldRight(empty[B])((h, t) => cons[B](f(h), t))
+
+    def filter(f: A => Boolean): Stream[A] = foldRight(empty[A])((h, t) =>
+      if (f(h))
+        cons(h, t)
+      else
+        t
+    )
+
+    def flatMap[B](f: A => Stream[B]): Stream[B] =
+      foldRight(empty[B])((h, t) => f(h).append(t))
+
+    def append[B >: A](s: => Stream[B]): Stream[B] = this match {
+      case Empty => s
+      case Cons(h, t) => cons[B](h(), t().append(s))
+    }
+
   }
 
   case object Empty extends Stream[Nothing]
@@ -51,5 +93,5 @@ object Chapter5 {
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
-    if (as.isEmpty) empty else Stream.cons(as.head, apply(as.tail: _*))
+    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
 }
--- src/test/scala/fpinscala/Chapter5Test.scala
+++ src/test/scala/fpinscala/Chapter5Test.scala
@@ -1,14 +1,22 @@
 package fpinscala
 
-import fpinscala.Chapter4._
 import org.scalatest.{FunSpec, Matchers}
 
-import scala.Predef.augmentString
 
 import Chapter5._
 class Chapter5Test extends FunSpec with Matchers {
 
   val testStream = Cons(() => 10, () => Cons(() => 11, () => Cons(() => 12, () => Empty)))
+
+  describe("headOption()") {
+    it("should return None on Empty") {
+      Empty.headOption shouldBe None
+    }
+
+    it("should return Some on non-Empty") {
+      testStream.headOption shouldBe Some(10)
+    }
+  }
 
   describe("toList()") {
 
@@ -22,7 +30,7 @@ class Chapter5Test extends FunSpec with Matchers {
 
   }
 
-  describe("take(), drop(), takeWhile()") {
+  describe("Exercise 5.2, 5.2: take(), drop(), takeWhile()") {
     it("should work for take(0)") {
       testStream.take(0) shouldBe Chapter5.Empty
     }
@@ -54,6 +62,78 @@ class Chapter5Test extends FunSpec with Matchers {
     it("should for takeWhile(n <= 11)") {
       testStream.takeWhile(_ <= 11).toList shouldBe List(10, 11)
     }
+  }
+
+  describe("Exercise 5.4: forAll()") {
+    it("should detect that all elements satisfy the condition") {
+      testStream.forAll(_ > 0) shouldBe true
+    }
+    it("should detect that some elements don't satisfy the condition") {
+      testStream.forAll(_ > 11) shouldBe false
+    }
+    it("should not evaluate all the elements if the condition is broken half-way") {
+      var executed = false
+      val lastElement = () => {
+        executed = true
+        14
+      }
+      Cons(() => 12, () => Cons(() => 13, () => Cons(lastElement, () => Empty)))
+        .forAll(_ < 13) shouldBe false
+
+      executed shouldBe false
+    }
+  }
+
+  describe("Exercise 5.7: map, filter, flatMap") {
+    it("map should not evaluate elements when not needed") {
+      var executed = false
+      val mappedStream = testStream.map { i =>
+        if (i > 11) {
+          executed = true
+        }
+        i * 2
+      }
+
+      mappedStream.take(1).toList shouldBe List(20)
+      executed shouldBe false
+    }
+
+    it("filter should not evaluate elements when not needed") {
+      var executed = false
+      val mappedStream = testStream.filter { i =>
+        if (i > 11) executed = true
+        i <= 10
+      }
+
+      mappedStream.take(1).toList shouldBe List(10)
+      executed shouldBe false
+    }
+
+    it("flatMap should work") {
+      var executed = false
+      val mappedStream = testStream.flatMap { i =>
+        if (i > 10) executed = true
+        testStream.map(_ + i)
+      }
+
+      mappedStream.take(1).toList shouldBe List(20)
+      executed shouldBe false
+    }
+
+    it("append should work") {
+      testStream.append(testStream).toList shouldBe List(10, 11, 12, 10, 11, 12)
+    }
+
+    it("append should be lazy") {
+      var executed = false
+      testStream.append({
+        executed = true
+        testStream
+      }).take(1).toList shouldBe List(10)
+
+      executed shouldBe false
+    }
+
   }
 
 }
